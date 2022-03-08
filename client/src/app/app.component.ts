0227ff6f24a2fa3cd8c19edfbda56cb0cb7188c6
ademgetahun <adonaymk2019@gmail.com>
2022-03-08 17:21:52 -0600

    Creating product interface using typescript

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,17 +11,18 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   title = 'AdemEcommerce'; //title first, constructor and ngOnInit last using angualr style guid
-  products: any[];
+  products: IProduct[];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5000/api/products?pageSize=50').subscribe((response: any) => {
-      //console.log(response);
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
+    this.http.get('https://localhost:5000/api/products?pageSize=50').subscribe(
+      (response: IPagination) => {
+        //console.log(response);
+        this.products = response.data;
+      }, error => {
+        console.log(error);
+      });
 
   }
 
