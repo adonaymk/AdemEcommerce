d18c294f04a9d2b8492303bf534e27655fd4ec79
ademgetahun <adonaymk2019@gmail.com>
2022-03-08 16:06:18 -0600

    Adding angular navigation bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AdemEcommerce';
+export class AppComponent implements OnInit {
+
+  title = 'AdemEcommerce'; //title first, constructor and ngOnInit last using angualr style guid
+  products: any[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5000/api/products?pageSize=50').subscribe((response: any) => {
+      //console.log(response);
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+
+  }
+
 }
